cc0f61b4307bd09e6884fd54570170033789d617
Jensssen <soeren.erichsen@gmx.de>
2025-02-01 11:48:00 +0100

    add test script

---
+++ raspberrypi/alarm_transmitter/main.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if GPIO.input(26) == GPIO.HIGH:
+        print("Button was pushed!")
+    else:
+        print("Button was not pushed!")
+    time.sleep(0.5)

96189c1b5f20b18ceabda9014306744b35f0ba21
Jensssen <soeren.erichsen@gmx.de>
2025-02-05 22:42:13 +0100

    add docker ignore

--- app/src/config.py
+++ app/src/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_PUBLIC_KEY: str
     JWT_ALGORITHM: str
-    ALARM_API_KEY: str
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
